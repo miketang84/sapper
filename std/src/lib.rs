f1725063a45f76a881b343bb8726fa1b16d28afd
Mike Tang <daogangtang@gmail.com>
2019-10-21 18:32:41 +0800

    std: rename ext_type! and ext_type_owned! to get_ext! and get_ext_owned!.
    Signed-off-by: Mike Tang <daogangtang@gmail.com>

--- std/src/lib.rs
+++ std/src/lib.rs
@@ -20,7 +20,7 @@ pub fn init(req: &mut Request, cookie_key: Option<&'static str>) -> Result<()> {
     sapper_query::parse(req)?;
     sapper_body::parse(req)?;
     sapper_session::session_val(req, cookie_key)?;
-    
+
     Ok(())
 }
 
@@ -37,39 +37,39 @@ pub fn finish(req: &Request, res: &mut Response) -> Result<()> {
 #[macro_export]
 macro_rules! res_redirect {
     ($redirect_uri:expr) => ({
-        use sapper::Response;
-        use sapper::status;
-        use sapper::header::Location;
+	use sapper::Response;
+	use sapper::status;
+	use sapper::header::Location;
 
-        let mut response = Response::new();
+	let mut response = Response::new();
 
-        response.set_status(status::Found);
-        //response.set_status(status::TemporaryRedirect);
-        response.headers_mut().set(Location($redirect_uri.to_owned()));
-        response.write_body(format!("redirect to {}", $redirect_uri));
+	response.set_status(status::Found);
+	//response.set_status(status::TemporaryRedirect);
+	response.headers_mut().set(Location($redirect_uri.to_owned()));
+	response.write_body(format!("redirect to {}", $redirect_uri));
 
-        Ok(response)
+	Ok(response)
     })
 }
 
 #[macro_export]
 macro_rules! res_400 {
     ($info:expr) => ({
-        Err(sapper::Error::Break($info.to_string()))
+	Err(sapper::Error::Break($info.to_string()))
     })
 }
 
 #[macro_export]
 macro_rules! res_404 {
     () => ({
-        Err(sapper::Error::NotFound)
+	Err(sapper::Error::NotFound)
     })
 }
 
 #[macro_export]
 macro_rules! res_500 {
     ($info:expr) => ({
-        Err(sapper::Error::InternalServerError($info.to_string()))
+	Err(sapper::Error::InternalServerError($info.to_string()))
     })
 }
 
@@ -77,13 +77,13 @@ macro_rules! res_500 {
 #[macro_export]
 macro_rules! set_response_redirect {
     ($response:expr, $redirect_uri:expr) => ({
-        use sapper::status;
-        use sapper::header::Location;
+	use sapper::status;
+	use sapper::header::Location;
 
-        $response.set_status(status::Found);
-        //response.set_status(status::TemporaryRedirect);
-        $response.headers_mut().set(Location($redirect_uri.to_owned()));
-        $response.write_body(format!("redirect to {}", $redirect_uri));
+	$response.set_status(status::Found);
+	//response.set_status(status::TemporaryRedirect);
+	$response.headers_mut().set(Location($redirect_uri.to_owned()));
+	$response.write_body(format!("redirect to {}", $redirect_uri));
     })
 }
 
@@ -92,15 +92,15 @@ macro_rules! set_response_redirect {
 #[macro_export]
 macro_rules! res_json {
     ($json:expr) => ({
-        use serde_json;
-        use sapper::Response;
-        use sapper::header::ContentType;
+	use serde_json;
+	use sapper::Response;
+	use sapper::header::ContentType;
 
-        let mut response = Response::new();
-        response.headers_mut().set(ContentType::json());
-        response.write_body(serde_json::to_string(&$json).unwrap());
+	let mut response = Response::new();
+	response.headers_mut().set(ContentType::json());
+	response.write_body(serde_json::to_string(&$json).unwrap());
 
-        Ok(response)
+	Ok(response)
     })
 }
 
@@ -108,30 +108,30 @@ macro_rules! res_json {
 #[macro_export]
 macro_rules! res_json_ok {
     ($info:expr) => ({
-        use sapper::Response;
-        use serde_json;
-        
-        let json2ret = json!({
-            "success": true,
-            "info": $info
-        });
+	use sapper::Response;
+	use serde_json;
 
-        res_json!(json2ret)
+	let json2ret = json!({
+	    "success": true,
+	    "info": $info
+	});
+
+	res_json!(json2ret)
     })
 }
 
 #[macro_export]
 macro_rules! res_json_error {
     ($info:expr) => ({
-        use sapper::Response;
-        use serde_json;
-        
-        let json2ret = json!({
-            "success": false,
-            "info": $info
-        });
-        
-        res_json!(json2ret)
+	use sapper::Response;
+	use serde_json;
+
+	let json2ret = json!({
+	    "success": false,
+	    "info": $info
+	});
+
+	res_json!(json2ret)
     })
 }
 
@@ -140,16 +140,16 @@ macro_rules! res_json_error {
 #[macro_export]
 macro_rules! res_html {
     ($html:expr, $context:expr) => ({
-        use sapper::Response;
-        use sapper::header::ContentType;
+	use sapper::Response;
+	use sapper::header::ContentType;
 
-        let res_str = render($html, $context);
+	let res_str = render($html, $context);
 
-        let mut response = Response::new();
-        response.headers_mut().set(ContentType::html());
-        response.write_body(res_str);
+	let mut response = Response::new();
+	response.headers_mut().set(ContentType::html());
+	response.write_body(res_str);
 
-        Ok(response)
+	Ok(response)
     })
 }
 
@@ -157,22 +157,22 @@ macro_rules! res_html {
 #[macro_export]
 macro_rules! res_html_before {
     ($context:expr) => ({
-        Err(sapper::Error::CustomHtml($context))
+	Err(sapper::Error::CustomHtml($context))
     })
 }
 
 #[macro_export]
 macro_rules! res_xml_string {
     ($context:expr) => ({
-        use sapper::Response;
-        use sapper::status::StatusCode;
+	use sapper::Response;
+	use sapper::status::StatusCode;
 
-        let mut res = Response::new();
-        res.set_status(StatusCode::Ok);
-        res.headers_mut().set_raw("Content-Type", vec!["text/xml".as_bytes().to_vec()]);
-        res.write_body($context);
+	let mut res = Response::new();
+	res.set_status(StatusCode::Ok);
+	res.headers_mut().set_raw("Content-Type", vec!["text/xml".as_bytes().to_vec()]);
+	res.write_body($context);
 
-        Ok(res)
+	Ok(res)
     })
 }
 
@@ -182,23 +182,23 @@ macro_rules! res_xml_string {
 #[macro_export]
 macro_rules! get_params {
     ($req:expr, $tykey:ty) => ({
-        match $req.ext().get::<$tykey>() {
-            Some(params) => {
-                params
-            },
-            None => {
-                let info = "no params";
-                info!("{}", info);
-                return res_400!(info);
-            }
-        }
+	match $req.ext().get::<$tykey>() {
+	    Some(params) => {
+		params
+	    },
+	    None => {
+		let info = "no params";
+		info!("{}", info);
+		return res_400!(info);
+	    }
+	}
     })
 }
 
 #[macro_export]
 macro_rules! get_path_params {
     ($req:expr) => ({
-        get_params!($req, PathParams)
+	get_params!($req, PathParams)
     })
 }
 
@@ -206,60 +206,60 @@ macro_rules! get_path_params {
 #[macro_export]
 macro_rules! get_query_params {
     ($req:expr) => ({
-        get_params!($req, QueryParams)
+	get_params!($req, QueryParams)
     })
 }
 
 #[macro_export]
 macro_rules! get_form_params {
     ($req:expr) => ({
-        get_params!($req, FormParams)
+	get_params!($req, FormParams)
     })
 }
 
 #[macro_export]
 macro_rules! get_json_params {
     ($req:expr) => ({
-        match serde_json::from_value(get_params!($req, JsonParams).clone()) {
-            Ok(val) => val,
-            Err(_) => {
-                let info = "Json parameter not match to struct.";
-                info!("{}", info);
-                return res_400!(info);
-            }
-        }
+	match serde_json::from_value(get_params!($req, JsonParams).clone()) {
+	    Ok(val) => val,
+	    Err(_) => {
+		let info = "Json parameter not match to struct.";
+		info!("{}", info);
+		return res_400!(info);
+	    }
+	}
     })
 }
 
 #[macro_export]
 macro_rules! t_cond {
     ($bool:expr, $prompt:expr) => ({
-        match $bool {
-            true => (),
-            false => {
-                let info = format!("test param condition result: {}", $prompt);
-                info!("{}", info);
-                return res_400!(info);
-            }
-        }
+	match $bool {
+	    true => (),
+	    false => {
+		let info = format!("test param condition result: {}", $prompt);
+		info!("{}", info);
+		return res_400!(info);
+	    }
+	}
     })
 }
 
 #[macro_export]
 macro_rules! _missing_or_unrecognized {
     ($field:expr) => ({
-        let info = format!("missing or unrecognized parameter {}.", $field);
-        info!("{}",info);
-        return res_400!(info);
+	let info = format!("missing or unrecognized parameter {}.", $field);
+	info!("{}",info);
+	return res_400!(info);
     })
 }
 
 #[macro_export]
 macro_rules! _using_default {
     ($field:expr, $default:expr) => ({
-        info!("missing or unrecognized parameter {}, using default {}.", $field, $default);
-        // return default
-        $default
+	info!("missing or unrecognized parameter {}, using default {}.", $field, $default);
+	// return default
+	$default
     })
 }
 
@@ -270,10 +270,10 @@ macro_rules! _using_default {
 #[macro_export]
 macro_rules! t_param {
     ($params:expr, $field:expr) => ({
-        match $params.get($field) {
-            Some(ref astr) => &astr[0],
-            None =>  _missing_or_unrecognized! ($field)
-        }
+	match $params.get($field) {
+	    Some(ref astr) => &astr[0],
+	    None =>  _missing_or_unrecognized! ($field)
+	}
     })
 }
 
@@ -281,105 +281,103 @@ macro_rules! t_param {
 #[macro_export]
 macro_rules! t_param_default {
     ($params:expr, $field:expr, $default:expr) => ({
-        match $params.get($field) {
-            Some(ref astr) => &astr[0],
-            None =>  _using_default! ($field, $default)
-        }
+	match $params.get($field) {
+	    Some(ref astr) => &astr[0],
+	    None =>  _using_default! ($field, $default)
+	}
     })
 }
 
 #[macro_export]
 macro_rules! t_param_parse {
     ($params:expr, $field:expr, $tykey:ty) => ({
-        match $params.get($field) {
-            Some(ref astr) => {
-                let _t = &astr[0];
-                match _t.parse::<$tykey>() {
-                    Ok(output) => output,
-                    Err(_) => {
-                        let info = format!("parse parameter type error {}.", $field);
-                        info!("{}", info);
-                        return res_400!(info);
-                    }
-                }
-            },
-            None =>  _missing_or_unrecognized! ($field)
-        }
+	match $params.get($field) {
+	    Some(ref astr) => {
+		let _t = &astr[0];
+		match _t.parse::<$tykey>() {
+		    Ok(output) => output,
+		    Err(_) => {
+			let info = format!("parse parameter type error {}.", $field);
+			info!("{}", info);
+			return res_400!(info);
+		    }
+		}
+	    },
+	    None =>  _missing_or_unrecognized! ($field)
+	}
     })
 }
 
 #[macro_export]
 macro_rules! t_param_parse_default {
     ($params:expr, $field:expr, $tykey:ty, $default:expr) => ({
-        match $params.get($field) {
-            Some(ref astr) => {
-                let _t = &astr[0];
-                match _t.parse::<$tykey>() {
-                    Ok(output) => output,
-                    Err(_) => {
-                        _using_default! ($field, $default)
-                    }
-                }
-            },
-            None =>  _using_default! ($field, $default)
-        }
+	match $params.get($field) {
+	    Some(ref astr) => {
+		let _t = &astr[0];
+		match _t.parse::<$tykey>() {
+		    Ok(output) => output,
+		    Err(_) => {
+			_using_default! ($field, $default)
+		    }
+		}
+	    },
+	    None =>  _using_default! ($field, $default)
+	}
     })
 }
 
 #[macro_export]
 macro_rules! t_arr_param {
     ($params:expr, $field:expr) => ({
-        match $params.get($field) {
-            Some(ref arr) => *arr,
-            None =>  _missing_or_unrecognized! ($field)
-        }
+	match $params.get($field) {
+	    Some(ref arr) => *arr,
+	    None =>  _missing_or_unrecognized! ($field)
+	}
     })
 }
 
 #[macro_export]
 macro_rules! t_arr_param_default {
     ($params:expr, $field:expr, $default:expr) => ({
-        match $params.get($field) {
-            Some(ref arr) => *arr,
-            None =>  _using_default! ($field, $default)
-        }
+	match $params.get($field) {
+	    Some(ref arr) => *arr,
+	    None =>  _using_default! ($field, $default)
+	}
     })
 }
 
 #[macro_export]
 macro_rules! t_has_param {
     ($params:expr, $field:expr) => ({
-        match $params.get($field) {
-            Some(_) => true,
-            None => false 
-        }
+	match $params.get($field) {
+	    Some(_) => true,
+	    None => false
+	}
     })
 }
 
 // ============ Ext Type Params ============
 #[macro_export]
-macro_rules! ext_type {
+macro_rules! get_ext {
     ($req:expr, $tykey:ty) => ({
-        match $req.ext().get::<$tykey>() {
-            Some(entity) => {
-                Some(entity)
-            },
-            None => None
-        }
+	match $req.ext().get::<$tykey>() {
+	    Some(entity) => {
+		Some(entity)
+	    },
+	    None => None
+	}
     })
 }
 
 
 #[macro_export]
-macro_rules! ext_type_owned {
+macro_rules! get_ext_owned {
     ($req:expr, $tykey:ty) => ({
-        match $req.ext().get::<$tykey>() {
-            Some(entity) => {
-                Some(entity.clone())
-            },
-            None => None
-        }
+	match $req.ext().get::<$tykey>() {
+	    Some(entity) => {
+		Some(entity.clone())
+	    },
+	    None => None
+	}
     })
 }
-
-
